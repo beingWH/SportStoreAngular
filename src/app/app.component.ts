4f1c0b65502ad2c5b652fb5681fc820c2e94bd98
wanghuan <wanghuan_kf@smec-cn.com>
2018-08-01 16:36:44 +0800

    update code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.css']
+// })
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app',
+  template: `
+    <div class="bg-success p-a-1 text-md-center"> This is SportStore</div>`
 })
+
 export class AppComponent {
   title = 'SportStore';
 }
